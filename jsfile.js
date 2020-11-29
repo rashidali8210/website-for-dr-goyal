6ba29dd72f45032efa608bb80c8b73391722ad91
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2020-11-29 20:32:30 +0530

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,16 @@
+setInterval(first,3000);
+setInterval(second,5000);
+setInterval(third,7000);
+function first()
+{
+  document.getElementById("Slide").src="image/pic2.jpg";
+}
+function second()
+{ 
+  document.getElementById("Slide").src="image/food.jpg";
+
+}
+function third()
+{
+  document.getElementById("Slide").src="image/pic1.jpg";
+}
